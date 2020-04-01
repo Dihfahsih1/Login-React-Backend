00dd0d9617f18d378f58dea2f2f3c1cc9fe04c2f
Dihfahsih1 <dihfahsihm@gmail.com>
2020-04-01 16:19:49 +0300

    testing backend

--- Main.js
+++ Main.js
@@ -5,3 +5,7 @@ const mysql = require('mysql');
 const session = require('express-session');
 const mySQLStore = require('express-mysql-session')(session);
 const router = require('./Router');
+
+app.use(express.static(path.join(__dirname, 'build')))
+app.use(express.json());
+console.log("testing server")
