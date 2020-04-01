95b1e771ac878eaf480b921a35949fdb62e33d10
Dihfahsih1 <dihfahsihm@gmail.com>
2020-04-01 14:56:32 +0300

    first react backend commit

---
+++ build/precache-manifest.c34530a3f294522bface9e2bc8f38887.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b980a7724109a11cf625bacf57797d9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "eadb39bc270152216f94",
+    "url": "./static/css/main.a57798c0.chunk.css"
+  },
+  {
+    "revision": "f4c696af735ece184526",
+    "url": "./static/js/2.909c3656.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "./static/js/2.909c3656.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eadb39bc270152216f94",
+    "url": "./static/js/main.51fff182.chunk.js"
+  },
+  {
+    "revision": "2e131721303cad2322bf",
+    "url": "./static/js/runtime-main.400a1515.js"
+  }
+]);
